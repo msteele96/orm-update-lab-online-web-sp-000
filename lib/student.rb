d1e1744a3e9e472e21303ecac13d92615d4cb523
Marc Steele <marcsteele2014@gmail.com>
2021-01-19 20:16:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     self.new(name, grade, id)
   end
 
-  def self.find_by_name(name)
+  def self.find_by_name
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
