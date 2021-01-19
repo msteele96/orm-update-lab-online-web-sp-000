8e4baf2074b184b6561e64791e8cf1ec8e41ee3c
Marc Steele <marcsteele2014@gmail.com>
2021-01-19 19:56:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students VALUES (@name, @grade, NULL)
+      INSERT INTO students VALUES (@name, @grade)
     SQL
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT * FROM students WHERE name = @name")[0][0]
