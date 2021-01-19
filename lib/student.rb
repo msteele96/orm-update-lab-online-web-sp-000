b58bf1ae2ac1e051a8a5b835f48fa0bc0b0200ee
Marc Steele <marcsteele2014@gmail.com>
2021-01-19 20:16:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).select
+    DB[:conn].execute(sql, name).map { |row|  }
   end
 
   def update
